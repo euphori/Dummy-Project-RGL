2dc8f7304446dfc3a655c7dc74a529d9ab767620
euphori <56996213+euphori@users.noreply.github.com>
2024-05-25 14:24:30 +0800

    initial commit

---
+++ data/scripts/main.ts
@@ -0,0 +1,7 @@
+import * as mc from "@minecraft/server";
+
+mc.system.runInterval(() => {
+  mc.world.getAllPlayers().forEach((player) => {
+   //player.runCommandAsync(`say ${player.getProperty("dummy:atk_state")}`);
+  });
+}, mc.TicksPerSecond);
