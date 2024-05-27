ed457bb70ca8d7bf1070541180a1bf6fed68195a
euphori <56996213+euphori@users.noreply.github.com>
2024-05-27 21:59:00 +0800

    added 2 entities and fixed animation bug
    note: dont ignore error logs

--- data/scripts/main.ts
+++ data/scripts/main.ts
@@ -2,6 +2,6 @@ import * as mc from "@minecraft/server";
 
 mc.system.runInterval(() => {
   mc.world.getAllPlayers().forEach((player) => {
-   //player.runCommandAsync(`say ${player.getProperty("dummy:atk_state")}`);
+  // player.runCommandAsync(`say ${player.getProperty("dummy:atk_state")}`);
   });
 }, mc.TicksPerSecond);
